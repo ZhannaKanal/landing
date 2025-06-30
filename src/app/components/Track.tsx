f114dd57e2779ee8cb015513d10e2e4dc0717e99
Zhanna <kezhanna.03@mail.ru>
2025-06-30 15:41:45 +0500

    added track

--- src/app/components/Track.tsx
+++ src/app/components/Track.tsx
@@ -1,7 +1,15 @@
-export default function Track(){
+import Image from "next/image";
+
+export default function Track() {
   return (
-    <div>
-      Hi
+    <div className="max-w-[1100px] w-full mx-auto flex justify-between items-center my-[100px]">
+      <div className=" text-start">
+        <p className="text-[32px] font-bold leading-9">Get a Clear View of Your <br /> Monthly Expenses</p>
+        <p className="text-[#252b3b] leading-7 pt-5">Easily track and categorize your spending to stay on top of your <br /> finances. Visualize your budget and savings at a glance.</p>
+      </div>
+      <div>
+        <Image src='/images/t1.avif' alt='' width={503} height={367}></Image>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,17 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import LogoList from "./components/LogoList";
-
+import Track from "./components/Track";
 
 export default function HomePage() {
   return (
     <>
-      <div  className="relative z-[-1] bg-[url('/images/i1.avif')] bg-cover bg-center h-[vh]">
+      <div className="relative z-[-1] bg-[url('/images/i1.avif')] bg-cover bg-center h-[vh]">
         <Header></Header>
         <HeroSection></HeroSection>
       </div>
       <LogoList></LogoList>
-
-
+      <Track></Track>
     </>
   );
 }
-
